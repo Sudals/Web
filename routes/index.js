371189507b179c993d2257e4613238e235f9fb7f
UBC <44963305+Sudals@users.noreply.github.com>
2023-09-09 18:51:13 +0900

    test

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '익스프레이이이이잉ㅇㅞ',pageName:'form.ejs' });
-});
-
-module.exports = router;
