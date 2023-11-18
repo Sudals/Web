8dd87b462b405ef809970893a8c9b021e8c08d04
UBC <44963305+Sudals@users.noreply.github.com>
2023-11-18 18:14:04 +0900

    week12
    12

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,8 @@ router.get('/',function(req,res){
 router.get('/insert',function(req,res){
     res.render('index',{title: '글쓰기',pageName:'posts/insert.ejs'})
 });
+router.get('/read',function(req,res){
+    const id=req.query.id;
+    res.render('index',{title: '게글정',pageName:'posts/read.ejs', id:id})
+});
 module.exports = router;
